4bd054daaf549d5e941ee1e8e5e2e28577341dc1
Harshad Sharma <harshad@sharma.io>
2022-03-25 10:39:41 +0530

    Bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Freedive.MixProject do
   def project do
     [
       app: :freedive,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
