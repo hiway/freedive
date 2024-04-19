32c3b6488ddb9302ac5d68337e88478ce8690103
Harshad Sharma <harshad@sharma.io>
2024-04-20 01:08:42 +0530

    Setup node name and cookie

--- lib/freedive_web/controllers/page_controller.ex
+++ lib/freedive_web/controllers/page_controller.ex
@@ -4,6 +4,7 @@ defmodule FreediveWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    node_name = Node.self() |> to_string()
+    render(conn, :home, layout: false, node_name: node_name)
   end
 end
