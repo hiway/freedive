3082e5eb8f5dcd90c43e4b358595091a9ed87afd
Harshad Sharma <harshad@sharma.io>
2024-04-19 21:28:59 +0530

    Enable TLS

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,4 +1,5 @@
 import Config
+import IP
 
 # config/runtime.exs is executed for all environments, including
 # during releases. It is executed after compilation and before the
@@ -44,54 +45,26 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || "localhost"
+  bind = System.get_env("BIND") || "127.0.0.1"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :freedive, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :freedive, FreediveWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
-    http: [
+    url: [host: host, port: port, scheme: "https"],
+    https: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+      ip: IP.from_string!(bind),
+      port: port,
+      cipher_suite: :strong,
+      keyfile: System.get_env("TLS_KEY_PATH"),
+      certfile: System.get_env("TLS_CERT_PATH"),
     ],
     secret_key_base: secret_key_base
-
-  # ## SSL Support
-  #
-  # To get SSL working, you will need to add the `https` key
-  # to your endpoint configuration:
-  #
-  #     config :freedive, FreediveWeb.Endpoint,
-  #       https: [
-  #         ...,
-  #         port: 443,
-  #         cipher_suite: :strong,
-  #         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-  #         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-  #       ]
-  #
-  # The `cipher_suite` is set to `:strong` to support only the
-  # latest and more secure SSL ciphers. This means old browsers
-  # and clients may not be supported. You can set it to
-  # `:compatible` for wider support.
-  #
-  # `:keyfile` and `:certfile` expect an absolute path to the key
-  # and cert in disk or a relative path inside priv, for example
-  # "priv/ssl/server.key". For all supported SSL configuration
-  # options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
-  #
-  # We also recommend setting `force_ssl` in your config/prod.exs,
-  # ensuring no data is ever sent via http, always redirecting to https:
-  #
-  #     config :freedive, FreediveWeb.Endpoint,
-  #       force_ssl: [hsts: true]
-  #
-  # Check `Plug.SSL` for all available options in `force_ssl`.
 
   # ## Configuring the mailer
   #
