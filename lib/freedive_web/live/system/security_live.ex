e7f02e9f3e086276f644c1690cb17954c5e727c6
Harshad Sharma <harshad@sharma.io>
2024-04-25 02:17:31 +0530

    Add design stubs for system/security

--- lib/freedive_web/live/system/security_live.ex
+++ lib/freedive_web/live/system/security_live.ex
@@ -8,9 +8,49 @@ defmodule FreediveWeb.SystemSecurityLive do
 
   def render(assigns) do
     ~H"""
-    <.title>
-      System Security
-    </.title>
+    <.panel search="true">
+      <:heading>
+        Security
+      </:heading>
+
+      <:tabs>
+        <.link href="#" class="is-active">All</.link>
+        <.link href="#">Enabled</.link>
+        <.link href="#">Disabled</.link>
+      </:tabs>
+
+      <.panel_block icon="hero-shield-exclamation" icon_class="has-text-danger">
+        SSH: Permit password login
+      </.panel_block>
+
+      <.panel_block icon="hero-shield-exclamation" icon_class="has-text-danger">
+        Firewall: Allow outgoing traffic
+      </.panel_block>
+
+      <.panel_block icon="hero-shield-check" icon_class="has-text-success">
+        DNS: Queries encrypted
+      </.panel_block>
+
+      <.panel_block icon="hero-shield-check" icon_class="has-text-success">
+        DNS: Blocklists enabled
+      </.panel_block>
+
+      <.panel_block icon="hero-shield-check" icon_class="has-text-success">
+        Firewall: Restrict incoming traffic
+      </.panel_block>
+
+      <.panel_block icon="hero-shield-check" icon_class="has-text-success">
+        OS: Up to date
+      </.panel_block>
+
+      <.panel_block icon="hero-shield-check" icon_class="has-text-success">
+        SSH: Disable root login
+      </.panel_block>
+
+      <.panel_block icon="hero-shield-check" icon_class="has-text-success">
+        Vuln: Heartbleed mitigation
+      </.panel_block>
+    </.panel>
     """
   end
 end
