092c04948de48fa4633dd7626db71e48518a3f51
Harshad Sharma <harshad@sharma.io>
2024-04-25 00:51:15 +0530

    Add placeholder design to system preferences and secrets pages

--- lib/freedive_web/live/system/preferences_live.ex
+++ lib/freedive_web/live/system/preferences_live.ex
@@ -8,9 +8,33 @@ defmodule FreediveWeb.SystemPreferencesLive do
 
   def render(assigns) do
     ~H"""
-    <.title>
-      System Preferences
-    </.title>
+    <.panel search="true">
+      <:heading>
+        System Preferences
+      </:heading>
+
+      <:tabs>
+        <.link href="#" class="is-active">All</.link>
+        <.link href="#">Changed</.link>
+        <.link href="#">Default</.link>
+      </:tabs>
+
+      <.panel_block icon="hero-clock">
+        Date & Time Sync
+      </.panel_block>
+
+      <.panel_block icon="hero-at-symbol">
+        DNS Resolvers
+      </.panel_block>
+
+      <.panel_block icon="hero-bolt">
+        Power Management
+      </.panel_block>
+
+      <.panel_block icon="hero-globe-alt">
+        Timezone
+      </.panel_block>
+    </.panel>
     """
   end
 end
--- lib/freedive_web/live/system/secrets_live.ex
+++ lib/freedive_web/live/system/secrets_live.ex
@@ -8,9 +8,62 @@ defmodule FreediveWeb.SystemSecretsLive do
 
   def render(assigns) do
     ~H"""
-    <.title>
-      System Secrets
-    </.title>
+    <.panel search="true">
+      <:heading>
+        Secrets
+      </:heading>
+
+      <:tabs>
+        <.link href="#" class="is-active">All</.link>
+        <.link href="#">API</.link>
+        <.link href="#">SSH</.link>
+        <.link href="#">Pwnd</.link>
+      </:tabs>
+
+      <.panel_block icon="hero-key">
+        Backblaze B2 AppID
+      </.panel_block>
+
+      <.panel_block icon="hero-key">
+        Backblaze B2 AppKey
+      </.panel_block>
+
+      <.panel_block icon="hero-shield-exclamation" icon_class="has-text-danger" active="true">
+        Telegram Bot Token
+      </.panel_block>
+
+      <.panel_block icon="hero-check-badge">
+        admin@desktop
+      </.panel_block>
+    </.panel>
+
+    <.box>
+      <form>
+        <label class="label">
+          Telegram Bot Token
+        </label>
+        <div class="field has-addons">
+          <div class="control is-expanded has-icons-left">
+            <input
+              class="input"
+              type="text"
+              placeholder="Token"
+              value="123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"
+            />
+            <span class="icon is-left">
+              <.icon name="hero-key" />
+            </span>
+          </div>
+
+          <div class="control">
+            <button class="button is-primary">Update</button>
+          </div>
+        </div>
+      </form>
+      <p>
+        <.link class="has-text-danger">This secret might be exposed, change immediately.</.link>
+      </p>
+    </.box>
     """
   end
 end
