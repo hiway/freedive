862eae68bc5b85bea3f788ab9c73279d12e1f7dd
Harshad Sharma <harshad@sharma.io>
2024-04-24 20:23:35 +0530

    Start :os_mon for live dashboard

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Freedive.MixProject do
   def application do
     [
       mod: {Freedive.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
